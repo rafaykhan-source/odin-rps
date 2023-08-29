6bb274f536a6ae9f2688d114d21a9f2b70174e04
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2023-08-29 00:15:07 -0400

    Add getComputerChoice function

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 "use strict";
 
+/**
+ * Returns a pseudorandom selection of "ROCK", "PAPER", or "SCISSORS".
+ * @returns {string}
+ */
+function getComputerChoice() {
+  const choices = ["ROCK", "PAPER", "SCISSORS"];
+
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
 function main() {
     console.log("Hello World");
 }
