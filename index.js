bfa040b244d3342e17303954a53fa3ee94e18d0f
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2023-08-29 00:15:25 -0400

    Add playRound function and logic

--- index.js
+++ index.js
@@ -10,6 +10,30 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
+/**
+ * Returns the result of the rock, paper, scissors round.
+ * @param {string} playerChoice Player's choice of rock, paper, or scissors.
+ * @param {string} computerChoice Computer's choice of rock, paper, or scissors.
+ * @returns {string}
+ */
+function playRound(playerChoice, computerChoice) {
+  const ruleSet = {
+    ROCK: "PAPER",
+    PAPER: "SCISSORS",
+    SCISSORS: "ROCK",
+  };
+
+  let result = "";
+  if (playerChoice == computerChoice) {
+    result = `${playerChoice} and ${computerChoice}. It's a tie!`;
+  } else if (ruleSet[playerChoice] == computerChoice) {
+    result = `You Lose! ${computerChoice} beats ${playerChoice}.`;
+  } else {
+    result = `You Win! ${playerChoice} beats ${computerChoice}.`;
+  }
+  return result;
+}
+
 function main() {
     console.log("Hello World");
 }
